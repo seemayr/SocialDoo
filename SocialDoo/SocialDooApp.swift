4f84a8d25ce6042c3a0328be0633bb3c47a441b6
Dominik Seemayr <seemayr@me.com>
2024-03-12 13:24:25 +0100

    Initial Commit

---
+++ SocialDoo/SocialDooApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialDooApp.swift
+//  SocialDoo
+//
+//  Created by Dominik Seemayr on 12.03.24.
+//
+
+import SwiftUI
+
+@main
+struct SocialDooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
